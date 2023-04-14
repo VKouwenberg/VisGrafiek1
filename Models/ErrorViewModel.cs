1dcd5ac9aeb407240710a553f827a29e5632ba4b
Valentijn <theappleloaf@gmail.com>
2023-04-14 12:45:27 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VisGrafiek1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
